28e88003f7531b682d4b2fb21ade35ac3723b821
Sefir <sebastian.firlik@student.put.poznan.pl>
2017-04-28 23:28:47 +0200

    Create Hello World Spring Boot application

---
+++ src/main/java/com/sefir/app/PlanerApplication.java
@@ -0,0 +1,17 @@
+package com.sefir.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PlanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanerApplication.class, args);
+	}
+}
